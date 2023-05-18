ecf6798fc1a424315a979b1b2d16769199574a42
patigayon-jason <jasonpatigayon25@gmail.com>
2023-05-18 09:04:58 +0800

    ExpoApp Update

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Button, StyleSheet } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => (
+  <View style={styles.container}>
+    <Button title="REGISTER" onPress={() => alert("Register button clicked")} />
+    <Button title="LOGIN" onPress={() => alert("Login button clicked")} />
+  </View>
+);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#E3FCE9",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
+
+export default App;
