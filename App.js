e1cee7cd2ab33201826a7dd41c14bf9496fdb346
JASON PATIGAYON <jasonpatigayon25@gmail.com>
2023-06-05 21:41:36 +0800

    SYSARCH - LABACT EXPO ACTIVITY

--- App.js
+++ App.js
@@ -1,19 +1,79 @@
-import React from "react";
-import { View, Button, StyleSheet } from "react-native";
+import React from 'react';
+import { View, Text, Button, StyleSheet, Image, Dimensions, TouchableOpacity } from 'react-native';
 
-const App = () => (
-  <View style={styles.container}>
-    <Button title="REGISTER" onPress={() => alert("Register button clicked")} />
-    <Button title="LOGIN" onPress={() => alert("Login button clicked")} />
-  </View>
-);
+const App = ({ navigation }) => {
+  const windowWidth = Dimensions.get('window').width;
+  const windowHeight = Dimensions.get('window').height;
+
+  const handleButtonPress = () => {
+    alert("Log In button clicked");
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.contentContainer}>
+        <Image
+          source={require('./assets/AppLogo.png')}
+          style={[styles.logo, { width: 320, height: 70 }]}
+        />
+        <Text style={styles.title}>Welcome to ECOMercado!</Text>
+        <Text style={styles.subtitle}>
+          Shop and donate to reduce waste and support a sustainable future.
+        </Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button
+          title="Get Started"
+          onPress={() => alert("Register button clicked")}
+          style={[styles.button, { width: 300, height: 50 }]}
+          color="#05652D"
+        />
+        <TouchableOpacity onPress={handleButtonPress} style={[styles.button, { width: 300, height: 50 }]}>
+          <Text style={styles.link}>I Already Have An Account, Log In</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#E3FCE9",
-    justifyContent: "center",
-    alignItems: "center",
+    backgroundColor: '#E3FCE9',
+  },
+  contentContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+  },
+  logo: {
+    marginBottom: 20,
+  },
+  title: {
+    fontWeight: 'bold',
+    color: '#05652D',
+    fontSize: 24,
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+  subtitle: {
+    color: '#05652D',
+    fontSize: 18,
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  buttonContainer: {
+    paddingBottom: 30,
+    alignItems: 'center',
+  },
+  button: {
+    marginBottom: 10,
+  },
+  link: {
+    color: '#05652D',
+    textDecorationLine: 'underline',
+    textAlign: 'center',
   },
 });
 
